f21aea0b30b43d2857a8b11b9336b5b9e43864cd
jwshuff <16825267+JWShuff@users.noreply.github.com>
2021-07-13 16:23:42 -0400

    Started models.py

--- car_app/cars_and_brands/models.py
+++ car_app/cars_and_brands/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    pass
+
+
+class Car(models.Model):
+    brand = models.ForeignKey(
+        Brand,
+        on_delete=models.CASCADE,
+        related_name='cars'
+    )
+    name = models.CharField(max_length=200)
+    style = models.CharField(max_length=200)  # refactor to a category? SUV/Coupe/Etc?
